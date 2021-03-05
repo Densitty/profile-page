f5cfaa43b0eaa018880ed77ceb88ed680cb7ba8f
Densitty <dennis.brough@yandex.com>
2021-03-04 22:45:25 -0600

    creating the navigation page for both large and small screens

--- script/app.js
+++ script/app.js
@@ -1,10 +1,19 @@
 const hamburgerContainer = document.querySelector("#header__main-nav");
 const hamburger = document.querySelector(".header__main-nav--hamburger");
 const links = document.querySelectorAll(".header__main-nav--links li");
+const lines = document.querySelectorAll(".line");
+
+// console.log(hamburgerContainer);
+// console.log(lines);
 
 hamburger.addEventListener("click", () => {
-  hamburgerContainer.classList.toggle("clicked");
+  lines.forEach((line) => {
+    line.classList.toggle("active");
+  });
+
+  hamburgerContainer.classList.toggle("show-container");
+
   links.forEach((link) => {
-    link.classList.toggle("fade");
+    link.classList.toggle("show-links");
   });
 });
